d63c181c935785ecee9fbc8f8ebfd08bacf00d60
abhardwaj <abhrdwaj@infrablok.com>
2023-01-24 16:01:05 +0530

    added deployment script draft 1

---
+++ scripts/walletGen.js
@@ -0,0 +1,18 @@
+const ethers = require('ethers')
+
+
+async function createWallet() {
+const wallet = ethers.Wallet.createRandom()
+
+    
+    
+    console.log(wallet);
+    
+    return {
+        address: wallet.address,
+        seedPhrase: wallet.mnemonic.phrase,
+        privatekey: wallet.privateKey
+    };
+}
+
+exports.createWallet();
