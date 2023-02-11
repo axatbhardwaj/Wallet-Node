427262ce5e0f85c2c51aef4010b740f5b4328ef8
axatbhardwaj <axatbhardwaj@outlook.com>
2023-02-11 21:55:48 +0530

    added from mnemonic

--- scripts/walletGen.js
+++ scripts/walletGen.js
@@ -9,3 +9,14 @@ const wallet = ethers.Wallet.createRandom()
     };
 }
 createWallet();
+
+
+/*
+from mnemonic 
+
+const ethers = require('ethers');
+let mnemonic = "YOUR MNEMONIC";
+let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
+console.log(mnemonicWallet.privateKey)
+
+*/
